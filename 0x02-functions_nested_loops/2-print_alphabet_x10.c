ac46b28267b6badb34f889760f2a002e2b4e7488
Mikiyas <educationforlife424@gmail.com>
2023-12-19 19:56:39 +0300

    Initialization place changed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/*
+ * print_alphabet_x10 - a function to print 26 alphabets 10 times downward
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar("\n");
+	}
+}
