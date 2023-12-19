919ed822f1d35186966f9aae2e48f798a1ce97ed
Mikiyas <educationforlife424@gmail.com>
2023-12-19 20:43:22 +0300

    Alpha done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if `c` is a letter,
+ * lowercase or uppercase
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
+
+
+
 
