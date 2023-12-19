5ff479c1b2d8228d0e242f44a39a3c66e3302222
Mikiyas <educationforlife424@gmail.com>
2023-12-19 20:55:23 +0300

    Sign project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * print_sign - function that prints the sign of a number
+ *
+ * @n: checks input of function
+ *
+ * Return:
+ *	 -> returns 1 and prints + if n is > zero
+ *	 ->  returns 0 and prints 0 if n is zero
+ *	 -> returns -1 and prints - if n is < 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
