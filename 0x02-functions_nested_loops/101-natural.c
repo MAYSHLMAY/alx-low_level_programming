8dc5ed7beabb0c09d38c57dd60149657f0a2224b
Mikiyas <educationforlife424@gmail.com>
2023-12-20 01:29:43 +0300

    Natural number sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,15 @@
+#include "stdio.h"
+
+void main(void)
+{
+	int pt = 1024;
+	int sum = 0;
+	while (pt >= 3)
+	{
+		--pt;
+		if (pt % 3 == 0 || pt % 5 == 0)
+			sum += pt;
+	}
+	printf("%d", sum);
+}
 
