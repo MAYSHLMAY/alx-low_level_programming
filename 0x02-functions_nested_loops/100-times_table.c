de6ff14ff2bd1c19b1024f15bc3c258ede2d248c
Mikiyas <educationforlife424@gmail.com>
2023-12-20 01:08:51 +0300

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,41 @@
+#include "main.h"
 
+/**
+ * n_times_table - function that prints the n times table
+ *
+ * @n: n times table
+ *
+*/
+
+void times_table(int n)
+{
+	 int i, j;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			 for (j = 0; j <= n; j++)
+			{
+				int result = i * j;
+
+				if (result < 10)
+				{
+					 _putchar(' ');
+					 _putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					 _putchar((result % 10) + '0');
+				}
+				if (j != 9)
+				{
+					 _putchar(',');
+					 _putchar(' ');
+				}
+			}
+			 _putchar('\n');
+		}
+	}
+}
