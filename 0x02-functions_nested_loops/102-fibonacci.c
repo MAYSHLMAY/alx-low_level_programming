70283f3f5d362916fd640183a0a68f42e744be68
Mikiyas <educationforlife424@gmail.com>
2023-12-20 01:46:27 +0300

    First fibonacci done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,30 @@
+#include "stdio.h"
 
+
+int main(void)
+{
+	int first = 0;
+
+	int second = 1;
+
+	int sum, temp;
+
+	for (int i = 0; i < 50; i++)
+	{
+		sum = first + second;
+		printf("%d", sum);
+		if (i != 49)
+			printf("%s", ", ");
+		temp = first;
+		first = second;
+		second = sum'
+	}
+	return (0);
+}
+
+
+
+
+
+	return (0);
+}
