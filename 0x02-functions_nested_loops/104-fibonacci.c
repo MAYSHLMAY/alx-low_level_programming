815494e12cb8d562639d329fd77d7852aaa38d20
Mikiyas <educationforlife424@gmail.com>
2023-12-20 02:31:04 +0300

    Last fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,29 @@
+#include "stdio.h"
 
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	unsigned int a, b, c;
+
+	a = 1;
+	b = 2;
+	printf("%u, %u", a, b);
+	for (i = 0; i < 96; i++)
+	{
+		c = a + b;
+		printf(", %u", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
