be6f9d9b505abbb5d9355a2dc47d02fe33f583ca
Mikiyas <educationforlife424@gmail.com>
2023-12-21 18:30:55 +0300

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+
+void print_triangle(int size)
+{
+	int i,j;
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size; j > i; j--)
+			{
+				_putchar(' ');
+			}
+		_putchar('#');
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
