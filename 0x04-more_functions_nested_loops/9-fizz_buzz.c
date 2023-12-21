514dc8df42cb5fe898beeb1ca582d89fd40ec0e9
Mikiyas <educationforlife424@gmail.com>
2023-12-21 18:12:50 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int i;
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("%s ","FizzBuzz");
@@ -14,5 +14,6 @@ int main(void)
 		else
 			printf("%d ",i);
 	}
+	printf("Buzz\n");
 }
 
