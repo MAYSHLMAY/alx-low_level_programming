970abbf0775a053a203e7642d56f8d9cdf90bd3e
Mikiyas <educationforlife424@gmail.com>
2023-12-21 20:09:40 +0300

    Print number sucks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+
+
+void print_number(int n)
+{
+	int d;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-1) * n;
+	}
+	while (n / d >= 10)
+		d *= 10;
+	while (d > 0)
+	{
+		putchar(n / d + '0');
+		n %= d;
+		d /= 10;
+	}
+}
+
+
