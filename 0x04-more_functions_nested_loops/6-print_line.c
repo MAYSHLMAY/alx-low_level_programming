7b08ea033aeb7749dce70d4577aa59ce008224dd
Mikiyas <educationforlife424@gmail.com>
2023-12-21 17:38:55 +0300

    Print line with parameter of certain number

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,10 @@
 void print_line(int n)
 {
 	int i;
-	for (i = 0; i < n; i++)
-		_putchar('_');
-	_putchar('\n');
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }
