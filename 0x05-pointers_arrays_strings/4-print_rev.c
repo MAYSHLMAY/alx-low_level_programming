9369736d3001fef80980275945b72cbd618a14d6
Mikiyas <educationforlife424@gmail.com>
2024-01-02 21:59:35 +0300

    Put char to form string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: None
+*/
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	int i;
+
+	while (s[len] != '\0')
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
