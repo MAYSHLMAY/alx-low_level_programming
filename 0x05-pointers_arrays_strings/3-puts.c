3596a1fe9c45fd0e108e24e4cdb090ec49dc1734
Mikiyas <educationforlife424@gmail.com>
2024-01-02 21:31:59 +0300

    Put char to form string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: None
+*/
+
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; *str != '\0'; str++)
+		_putchar(*str + 0);
+	_putchar('\n');
+}
