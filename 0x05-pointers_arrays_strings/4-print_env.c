0788155a107af1c4e624d026581d050c6deb0856
Mikiyas <educationforlife424@gmail.com>
2024-01-02 21:57:45 +0300

    Put char to form string

--- 0x05-pointers_arrays_strings/4-print_env.c
+++ 0x05-pointers_arrays_strings/4-print_env.c
@@ -10,18 +10,13 @@
 
 void print_rev(char *s)
 {
-	int len;
+	int len = 0;
 
 	int i;
 
-	chat *p = s;
-
-	while (*p != '\0')
-	{
-		p++;
+	while (s[len] != '\0')
 		len++;
-	}
 	for (i = len - 1; i >= 0; i--)
-		_putchar(*(s + i));
+		_putchar(s[i]);
 	_putchar('\n');
 }
