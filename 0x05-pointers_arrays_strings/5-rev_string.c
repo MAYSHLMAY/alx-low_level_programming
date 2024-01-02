04fe5a10661d452a05cbb0d54c192f1cf736da5e
Mikiyas <educationforlife424@gmail.com>
2024-01-02 22:14:31 +0300

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+void rev_string(char *s)
+{
+	int len = 0;
+
+	int i;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(*(s + i));
+}
