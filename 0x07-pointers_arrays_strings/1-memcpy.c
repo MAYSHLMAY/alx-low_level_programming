76631c64dbc52110d2730a0257bad2ece3a6912c
Mikiyas <educationforlife424@gmail.com>
2024-01-08 18:05:07 +0300

    Memory copy done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * _memcpy - memory copy
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (*dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
 #define _main_h__
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
