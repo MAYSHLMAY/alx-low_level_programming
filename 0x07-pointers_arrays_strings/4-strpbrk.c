479f0120233623ad81558a74aaf32d7db3cede12
Mikiyas <educationforlife424@gmail.com>
2024-01-08 21:01:23 +0300

    STRpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
 
+char *_strpbrk(char *s, char *accept)
+{
+
+	while (*s != '\0')
+	{
+		unsigned int c2 = 0;
+
+		while (accept[c2] != '\0')
+		{
+			if (accept[c2] == *s)
+				return (s);
+			c2++;
+		}
+		s++;
+	}
+	return (NULL);
+}
