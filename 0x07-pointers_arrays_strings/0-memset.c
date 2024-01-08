776f7dde8a290e2ccbe2efd668700807674cd64d
Mikiyas <educationforlife424@gmail.com>
2024-01-08 17:46:25 +0300

    Memset function created

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - function fills the first n bytes of the memory area
- *           pointed to by s with the constant byte b
- * @s: pointer to array of chars
- * @b: character to replace array of chars
+ * _memset - function that copy a certain certain char to contigous mem
+ * @s: pointer to char params
+ * @b: data to change
  * @n: index
  * Return: *s
- **/
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
