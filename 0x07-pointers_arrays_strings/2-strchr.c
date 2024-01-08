5501fc3704c29da7c56d889819dca4a51978d65a
Mikiyas <educationforlife424@gmail.com>
2024-01-08 18:19:19 +0300

    character pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * _strchr - prints found c
+ * @s: pointer to char
+ * @c: char param to found
+ * Return: *S
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else
+			return (NULL);
+		s++;
+	}
+}
