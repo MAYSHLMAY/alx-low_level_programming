f879a5121d11ce6702c67d703c2a516229d4e7d9
Mikiyas <educationforlife424@gmail.com>
2024-01-08 22:01:39 +0300

    Hay stack and needle

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,32 @@
+#include "main.h"
+#include <stddef.h>
 
+/**
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		unsigned int c1 = 0;
+		unsigned int c2 = 0;
+
+		while (needle[c2] != '\0' && haystack[c1] == needle[c2])
+		{
+			c1++;
+			c2++;
+		}
+
+		if (needle[c2] == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
