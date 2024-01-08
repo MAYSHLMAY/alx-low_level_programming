c7b44baead3b5408b3f30261cb8450977f923977
Mikiyas <educationforlife424@gmail.com>
2024-01-08 23:08:03 +0300

    Diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	int sum1, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - i - 1));
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
