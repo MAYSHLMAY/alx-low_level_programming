769f6f04fa4c2ff239bafca7429c00a20301f77b
Mikiyas <educationforlife424@gmail.com>
2024-01-08 19:20:44 +0300

    Str spn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+
+/**
+* _strspn - function Gets the length of a prefix substring.
+* @s: String where substring will look.
+* @accept: Substring of accepted chars.
+* Return: Length.
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c1 = 0;
+	while (*s != '\0')
+	{
+		unsigned int c2 = 0;
+		while (accept[c2] != '\0')
+		{
+			if (*s == accept[c2])
+			{
+				c1++;
+				break;
+			}
+			c2++;
+		}
+		if (accept[c2] == '\0')
+			break;
+		s++;
+	}
+	return (c1);
+}
