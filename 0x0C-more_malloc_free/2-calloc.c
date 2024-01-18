2e4d046d8f28ac9d31cf2488457be1080b7a4dbc
Mikiyas Dawit <educationforlife424@gmail.com>
2024-01-18 23:56:08 +0300

    memsetcand malloc to calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+#include "cstring.h"
 
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+        char *ptr;
+
+        if (nmemb == 0 || size == 0)
+                return (NULL);
+
+        ptr = malloc(size * nmemb);
+
+        if (ptr == NULL)
+                return (NULL);
+
+        memset(ptr, 0, nmemb * size);
+
+        return (ptr);
+}
