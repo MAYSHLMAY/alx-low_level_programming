613960fb21c5c3aaeade7b6865430580ca34a219
Mikiyas <educationforlife424@gmail.com>
2024-01-22 18:59:18 +0300

    Done with free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,20 @@
+#include "dog.h"
+#include "stdlib.h"
 
+/**
+ * free_dog - free struct pointer
+ *
+ * @d: pointer to free
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
+}
