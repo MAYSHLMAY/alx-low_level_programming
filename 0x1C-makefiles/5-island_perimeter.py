40f8646dce9654b147698db79b9d37bbb9fd57c2
Mikiyas dawit <educationforlife424@gmail.com>
2024-06-02 19:20:53 +0300

    griddy

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,29 +2,29 @@ def island_perimeter(grid):
     """Returns the perimeter of the island described in grid.
 
     Args:
-        grid (list): A list of lists of integers, where 0 represents water and 1 represents land.
+        grid (list): A list of lists of ints 0 and 1
 
     Returns:
         int: The perimeter of the island.
     """
     width = len(grid[0])
     height = len(grid)
-    perimeter = 0
+    perim = 0
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
                 # Check the left edge
                 if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
+                    perim += 1
                 # Check the right edge
                 if j == width - 1 or grid[i][j+1] == 0:
-                    perimeter += 1
+                    perim += 1
                 # Check the top edge
                 if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
+                    perim += 1
                 # Check the bottom edge
                 if i == height - 1 or grid[i+1][j] == 0:
-                    perimeter += 1
+                    perim += 1
 
-    return perimeter
+    return perim
