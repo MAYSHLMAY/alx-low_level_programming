291847e0eb6cc81d927312cc76b221905de7071c
Mikiyas dawit <educationforlife424@gmail.com>
2024-06-09 19:55:02 +0300

    Some change

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,23 @@
+#!/usr/bin/python3
+
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid.
-
+    """returns the perim of the island
     Args:
-        grid (list): A list of lists of integers
-
-    Returns:
-        int: The perimeter of the island.
+         grid (list) = list of integers
     """
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # Check the left edge
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                # Check the right edge
-                if j == len(grid[0]) - 1 or grid[i][j+1] == 0:
-                    perimeter += 1
-                # Check the top edge
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                # Check the bottom edge
-                if i == len(grid) - 1 or grid[i+1][j] == 0:
-                    perimeter += 1
-    return perimeter
+                size += 1
+                # checking for boundery or a lake
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
